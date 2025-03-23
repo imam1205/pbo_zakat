fccd46ffe173a7211f342dd5ee974e44cff2516b
TengkuAdindaSari <dinda.20305@gmail.com>
2025-03-23 16:59:50 +0700

    tambah data beras

--- zakat.py
+++ zakat.py
@@ -1,4 +1,4 @@
-import mysql.connector
+#import mysql.connector
 from datetime import datetime
 
 def connect_db():
@@ -24,3 +24,59 @@ def create_beras_table():
     db.commit()
 
 #beras db
+def tambah_data_beras():
+    try:
+        print("\n=== Tambah Data Beras ===")
+        harga = float(input("Masukkan harga beras per Liter: Rp "))
+        
+        cursor.execute('''
+            INSERT INTO beras (harga)
+            VALUES (%s)
+        ''', (harga,))
+        db.commit()
+        print("\nHarga beras berhasil ditambahkan!")
+        
+    except ValueError:
+        print("Error: Harga harus berupa angka!")
+    except Exception as e:
+        print(f"Error: {str(e)}")
+
+#function while
+    try:
+        db = connect_db()
+        cursor = db.cursor()
+        create_beras_table()
+        create_payment_table()
+    except mysql.connector.Error as e:
+        print(f"Database connection failed: {e}")
+        exit(1)
+    try:
+        while True:
+            print("\n=== Menu ===")
+            print("1. Tambah Data Beras")
+            print("2. Tampilkan Data Beras")
+            print("3. Tampilkan Data Pembayaran")
+            print("4. Pembayaran Zakat")
+            print("5. Export Laporan Excel")
+            print("6. Keluar")
+        
+            pilihan = input("Pilih menu (1-6): ")
+        
+            if pilihan == '1':
+                tambah_data_beras()
+            # elif pilihan == '2':
+            #     tampilkan_data_beras()
+            # elif pilihan == '3':
+            #      tampilkan_data_pembayaran()
+            # elif pilihan == '4':
+            #     tambah_pembayaran()
+            # elif pilihan == '5':
+                # generate_excel_report()
+            # elif pilihan == '6':
+            #      print("Program selesai")
+            #     break
+            # else:
+            #      print("Pilihan tidak valid!")
+    finally:
+        cursor.close()
+        db.close()
